28ed0eef27880a5d1df692868e2987a425a4599a
prabhjot-arora31 <83449967+prabhjot-arora31@users.noreply.github.com>
2023-07-01 13:42:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import Form from './src/components/Form';
+import Balloon from './src/components/Balloon';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const App = () => {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Balloon'>
+        <Stack.Screen name='Balloon' component={Balloon}/>
+        <Stack.Screen name='Form' component={Form}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+export default App;
